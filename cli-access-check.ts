6cbb7358da0398390b2977f78fbda86ef8ad3313
Trotyl <trotyl@qq.com>
2016-12-04 22:10:31 +0800

    Add check-access cli command.

---
+++ cli-access-check.ts
@@ -0,0 +1,21 @@
+// Load the SDK and UUID
+import * as AWS from 'aws-sdk'
+import * as uuid from 'uuid'
+
+// Create an S3 client
+const s3 = new AWS.S3()
+
+// Create a bucket and upload something into it
+const bucketName = `node-sdk-check-${uuid.v4()}`
+
+async function main() {
+    try {
+        await s3.createBucket({ Bucket: bucketName }).promise()
+        await s3.deleteBucket({ Bucket: bucketName }).promise()
+        console.log("Successfull")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+main()
